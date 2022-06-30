43293436eeb8d6889ee12e6d2e366ff79cf4c9c4
jomojay <djdrjay001@gmail.com>
2022-06-30 21:48:57 +0100

    corrected pycodestyle error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,9 @@
 from sys import argv
 
 if __name__ == "__main__":
-    l = len(argv)
-    print("{:d} {:s}{:s}".format(l - 1, "argument" if l == 2 else "arguments",
-                                "." if l == 1 else ":"))
+    c = len(argv)
+    print("{:d} {:s}{:s}".format(c - 1, "argument" if c == 2 else "arguments",
+                                        "." ifc == 1 else ":"))
     for i, s in enumerate(argv):
         if i > 0:
             print("{:d}: {:s}".format(i, s))

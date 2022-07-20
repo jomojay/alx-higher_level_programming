efdaab1cf399d81a51e2f72010848c02be41c8de
jomojay <djdrjay001@gmail.com>
2022-07-21 00:27:04 +0100

    corrected errors in the last task file

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -4,9 +4,9 @@ import math
 
 
 class MagicClass:
-    """This represents a circle"""
+    """The class for circle"""
     def __init__(self, radius=0):
-        """Initializes the Magic Class"""
+        """The __init__ method for MagicClass class"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
@@ -17,5 +17,5 @@ class MagicClass:
         return (self.__radius ** 2) * math.pi
 
     def circumference(self):
-        """Calculates the circumference of the circle"""
-        return 2 * math.pi * self.
+        """Calculates the perimeter of the circle"""
+        return 2 * math.pi * self.__radius

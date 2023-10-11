bbb77e1996486af085073a45afa46913066b325b
jomojay <djdrjay001@gmail.com>
2023-10-11 16:48:10 +0100

    fixed

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    def sr_elm(elm):
-        return (replace if elm == search else elm)
-    return list(map(sr_elm, my_list))
+    return list(map(lambda x: replace if x == search else x, my_list))

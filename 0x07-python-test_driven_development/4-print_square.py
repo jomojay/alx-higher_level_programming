7ae6bd0d8324f177d9a8c2177c139c84daeccf46
jomojay <djdrjay001@gmail.com>
2023-11-01 11:06:32 +0100

    fixed pycodestyle

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -17,6 +17,7 @@ def print_square(size):
         raise ValueError('size must be >= 0')
     print((("#" * size + "\n") * size), end="")
 
+
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/4-print_square.txt")

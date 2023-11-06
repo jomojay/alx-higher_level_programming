a9d926082b532de1dad16148f1a55875c0617b34
jomojay <djdrjay001@gmail.com>
2023-11-06 19:37:32 +0100

    removed import

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -12,8 +12,3 @@ class MyList(list):
             self (MyList object)
         """
         print(sorted(self))
-
-
-if __name__ == '__main__':
-    import doctest
-    doctest.testfile('tests/1-my_list.txt')

15045cb4b72bb2eb4efb77e8e0aad75c59b82f2e
jomojay <djdrjay001@gmail.com>
2023-12-13 13:00:14 +0100

    commit for all

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- create database
+-- CREATE DATABASE that never fails;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- 5-full_table.sql 
+-- prints full description about table 
+SHOW CREATE TABLE first_table;

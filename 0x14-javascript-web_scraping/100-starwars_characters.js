d63eadad543dc3db7f666c3d736c451226cbf673
jomojay <djdrjay001@gmail.com>
2024-04-23 22:28:29 +0100

    fixed 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // get starwars characters from a film
 const request = require('request');
-const url = 'h`ttps://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   if (error) throw error;
   for (const character of JSON.parse(body).characters) {
